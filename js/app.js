440fcc5cd6ecbad44325c55c09c80e4aea4ff126
Tech-A46 <tech-a46@Tech-a46s-MacBook-Air.local>
2018-09-11 19:01:32 +0100

    completed the first two bonus objectives

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ var num3=0.0;
 var ans=0.0;
 var oper="";
 var mode="";
+var stay='';
+var staynum=1;
 // functions
 function Add(num1,num2){
   ans = Number(num1) + Number(num2);
@@ -33,51 +35,76 @@ function sRoot(num1){
   ans=Math.sqrt(Number(num1));
   return ans;
 }
+function Advanced(){
+  oper=prompt('Do you want to find the [P]ower of a number or the s[q]uare root of a number?')
+  oper= oper.toLowerCase();
+  if (oper=="p"){
+    num1=prompt("enter the root number");
+    num2=prompt("enter the power number");
+    num3=Power(num1,num2);
+    alert("the Answer is "+num3);
+  } else if(oper=="q"){
+    num1=prompt("enter the number you want to square root");
+    num3=sRoot(num1);
+    alert("the Answer is "+num3);
+  }else{}
+}
+function Basic(){
+  num1=prompt("enter first number");
+  num2=prompt('enter second number');
+  oper=prompt("Do you want to [A]dd, [S]ubtract, [M]ulitply, or [D]ivide?");
+  oper =oper.toLowerCase();
+  switch (oper) {
+    case 'a':
+    num3 = Add(num1,num2);
+    alert("the Answer is "+num3);
+    break;
+    case 's':
+    num3 = subtract(num1,num2);
+    alert("the answer is "+num3);
+    break;
+    case 'm':
+    num3 = Multiply(num1,num2);
+    alert("the answer is "+num3);
+    break;
+    case 'd':
+    num3 = Divide(num1,num2);
+    alert("the answer is "+num3);
+    break;
+    default:
+  }
+}
 // main
-while(true){
   alert('welcome to the Calculator');
   mode=prompt("please choose mode: [A]davanced or [B]asic");
   mode = mode.toLowerCase();
   if (mode == "a"){
-    oper=prompt('Do you want to find the [P]ower of a number or the s[q]uare root of a number?')
-    oper= oper.toLowerCase();
-    if (oper=="p"){
-      num1=prompt("enter the root number");
-      num2=prompt("enter the power number");
-      num3=Power(num1,num2);
-      alert("the Answer is "+num3);
-    } else if(oper=="q"){
-      num1=prompt("enter the number you want to square root");
-      num3=sRoot(num1);
-      alert("the Answer is "+num3);
-    }else{}
+    do{
+      Advanced();
+      stay=prompt("To Recalculate type [y], to exit type[n]");
+      stay =stay.toLowerCase;
+      if (stay =="y"){
+        staynum = 1;
+      }else if(stay=="n"){
+        staynum =0;
+      }else{
+        staynum = 1;
+      }
+    }while(staynum>0);
 
   }else if (mode =="b"){
-    num1=prompt("enter first number");
-    num2=prompt('enter second number');
-    oper=prompt("Do you want to [A]dd, [S]ubtract, [M]ulitply, or [D]ivide?");
-    oper =oper.toLowerCase();
-    switch (oper) {
-      case 'a':
-      num3 = Add(num1,num2);
-      alert("the Answer is "+num3);
-      break;
-      case 's':
-      num3 = subtract(num1,num2);
-      alert("the answer is "+num3);
-      break;
-      case 'm':
-      num3 = Multiply(num1,num2);
-      alert("the answer is "+num3);
-      break;
-      case 'd':
-      num3 = Divide(num1,num2);
-      alert("the answer is "+num3);
-      break;
-      default:
-    }
-
+    do{
+      Basic();
+      stay=prompt("To Recalculate type [y], to exit type[n]");
+      stay =stay.toLowerCase;
+      if (stay =="y"){
+        staynum = 1;
+      }else if(stay=="n"){
+        staynum =0;
+      }else{
+        staynum = 1;
+      }
+    }while(staynum>0);
   }else{
     alert("Invalid Choice");
   }
-}
